aeff5f5276213d8bd654916ab2636cc9b2d99eff
Alek Michelson <am7590@rit.edu>
2023-04-22 15:40:57 -0400

    Initial Commit

---
+++ SucculentApp/SucculentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SucculentAppApp.swift
+//  SucculentApp
+//
+//  Created by Alek Michelson on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct SucculentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
